2083cdb4ee8db6f94f708f7e9778798f83c22a29
WindowsLogic <39615739+WindowsLogic@users.noreply.github.com>
2020-05-28 18:25:19 +0100

    Added project files

---
+++ Enlarged.vb
@@ -0,0 +1,33 @@
+﻿Public Class Enlarged
+    Dim StopWatch As New Diagnostics.Stopwatch
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim elapsed As TimeSpan = Me.StopWatch.Elapsed
+        Label1.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", Math.Floor(elapsed.TotalHours), elapsed.Minutes, elapsed.Seconds, elapsed.Milliseconds)
+    End Sub
+    Private Sub Start_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles StartButton.Click
+        If StartButton.Text = "Start" Then
+            Timer1.Start()
+            Me.StopWatch.Start()
+            StartButton.Text = "Stop"
+        Else
+            Timer1.Stop()
+            Me.StopWatch.Stop()
+            StartButton.Text = "Start"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ResetButton.Click
+        Me.StopWatch.Reset()
+        Label1.Text = "00:00:00:000"
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub Button3_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LapButton.Click
+        ListBox1.Items.Add(Label1.Text)
+    End Sub
+
+    Private Sub RestoreButton_Click(sender As Object, e As EventArgs) Handles RestoreButton.Click
+        Main.Show()
+        Me.Close()
+    End Sub
+End Class
---
+++ Main.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.ResetButton = New System.Windows.Forms.Button()
+        Me.LapButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.EnlargeButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(40, 54)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(156, 23)
+        Me.StartButton.TabIndex = 0
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'ResetButton
+        '
+        Me.ResetButton.Location = New System.Drawing.Point(121, 83)
+        Me.ResetButton.Name = "ResetButton"
+        Me.ResetButton.Size = New System.Drawing.Size(75, 23)
+        Me.ResetButton.TabIndex = 2
+        Me.ResetButton.Text = "Reset"
+        Me.ResetButton.UseVisualStyleBackColor = True
+        '
+        'LapButton
+        '
+        Me.LapButton.Location = New System.Drawing.Point(40, 83)
+        Me.LapButton.Name = "LapButton"
+        Me.LapButton.Size = New System.Drawing.Size(75, 23)
+        Me.LapButton.TabIndex = 3
+        Me.LapButton.Text = "Lap"
+        Me.LapButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(49, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(138, 25)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "00:00:00:000"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 114)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(212, 147)
+        Me.ListBox1.TabIndex = 5
+        '
+        'EnlargeButton
+        '
+        Me.EnlargeButton.Location = New System.Drawing.Point(149, 264)
+        Me.EnlargeButton.Name = "EnlargeButton"
+        Me.EnlargeButton.Size = New System.Drawing.Size(75, 23)
+        Me.EnlargeButton.TabIndex = 6
+        Me.EnlargeButton.Text = "Enlarge"
+        Me.EnlargeButton.UseVisualStyleBackColor = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(236, 299)
+        Me.Controls.Add(Me.EnlargeButton)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LapButton)
+        Me.Controls.Add(Me.ResetButton)
+        Me.Controls.Add(Me.StartButton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Main"
+        Me.Text = "Simple Stopwatch"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StartButton As Button
+    Friend WithEvents ResetButton As Button
+    Friend WithEvents LapButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents EnlargeButton As Button
+End Class
